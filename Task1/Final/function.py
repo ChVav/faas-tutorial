5e9d89bdc855777d298b4245afb8b0b1bc7b8849
Sashko Ristov <39643984+sashkoristov@users.noreply.github.com>
2022-09-23 11:08:13 +0200

    Task 1 input and final

--- Task1/Final/function.py
+++ Task1/Final/function.py
@@ -1,8 +1,13 @@
 def lambda_handler(event, context):
+
+    # Read inputs of the function
     a = event['a']
     b = event['b']
+
+    # Write the code of the function
     result = a + b
+
+    # Return the output
     return {
-        'statusCode': 200,
         'result': result
     }
---
+++ Task1/Initial/function.py
@@ -0,0 +1,8 @@
+def handler_name(event, context): # You can change the name of the handler
+    
+    # Read inputs of the function
+
+    # Write the code of the function
+
+    # Return the output
+
